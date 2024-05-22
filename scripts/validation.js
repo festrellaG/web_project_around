e8cd7836efa68fe81b93b821fdadb3f9bbff6fff
Fabian Eduardo Estrella Romero <festrella@aeromexico.com>
2024-05-22 14:22:13 -0600

    add validation.js

---
+++ scripts/validation.js
@@ -0,0 +1,93 @@
+const showInputError = (formElement, inputElement, errorMessage, settings) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(settings.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(settings.inputErrorActive);
+};
+
+const hideInputError = (formElement, inputElement, settings) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(settings.inputErrorClass);
+  errorElement.classList.remove(settings.inputErrorActive);
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement, settings) => {
+  if (!inputElement.validity.valid) {
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      settings
+    );
+  } else {
+    hideInputError(formElement, inputElement, settings);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement, settings) => {
+  //console.log(hasInvalidInput(inputList));
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(settings.inactiveButtonClass);
+  } else {
+    buttonElement.classList.remove(settings.inactiveButtonClass);
+  }
+  console.log("boton", buttonElement);
+};
+
+const setEventListeners = (formElement, settings) => {
+  //console.log("setEventListeners");
+  const inputList = Array.from(
+    formElement.querySelectorAll(settings.inputSelector)
+  );
+
+  const buttonElement = formElement.querySelector(
+    settings.submitButtonSelector
+  );
+
+  toggleButtonState(inputList, buttonElement, settings);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement, settings);
+      toggleButtonState(inputList, buttonElement, settings);
+    });
+  });
+};
+
+const enableValidation = (settings) => {
+  const formList = Array.from(document.querySelectorAll(settings.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement, settings);
+  });
+};
+
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_inactive",
+  inputErrorClass: "popup__input_type_error",
+  inputErrorActive: "popup__input-error_active",
+  errorClass: ".popup__error",
+});
+
+//-------------------------- Cerrar ventana emergente del popup
+const overlay = document.querySelector(".page");
+const popup = document.querySelector(".popup");
+
+overlay.addEventListener("change", function (event) {
+  console.log(event.target);
+  if (event.target === overlay) {
+    popup.style.display = "none";
+  }
+});
